bf99c4b01b2791b739b4811e93dfad24dfd1e086
Rohit Dhakad <rohitdhaka045@gmail.com>
2024-10-18 21:31:21 +0530

    first commit

---
+++ Internshala-Automation-main/data.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        College: "Institute of professional studies ,Indore",
+        Degree: "Bachelor of Technology (B.Tech)",
+        Stream: "Computer Science and Engineering",
+        Percentage: "7.92",
+        Training: "Data Structures and Algorithms, Web Development",
+        Organization: "Udemy.com.",
+        description: "Udemy is a great platform where I got to learn the not only the basics of data structures and algorithms but I also got familiar with Web Development and Competitive Programming. Here, the main focus is on concept building and it was a great start for me.",
+        link: "https://github.com/xyz/Task1",
+        hiringReason: "Over the past few years, I have acquired relevant skills and experience, which I shall bring to your organization. I have also worked tirelessly on my communication abilities and teamwork skills, which I will put to use in my future career, which would be in your organization if I am selected for the position.",
+        availability: "Yes, I will be available",
+        rating: "4",
+        cover_letter:"Sir i am big fan of your work. I want to work with your organisaton and i want to expand my knowledge"
+    }
+]
---
+++ Internshala-Automation-main/secret.js
@@ -0,0 +1,3 @@
+let id = "yp5253522@gmail.com";
+let pass = "Yash262692";
+module.exports = { id, pass };
